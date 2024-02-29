f8fc1c2f405e170a205ab7daae241dc431b03ff0
Sabin Mandal <sabin@Sabins-MacBook-Air.local>
2024-02-29 13:10:48 +0545

    Task3

---
+++ PersonalDetails/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Name : Pravesh Gaire");
+        System.out.println("Address : Shivaraj-5,Chandrauta,Kapilvastu,Nepal");
+        System.out.println("Age : 20");
+        System.out.println("Ph no. : 9857014043");
+    }
+}
