bba4fd278e4b68fd6189a7f37f652c2e1c6e69d8
Sabin Mandal <sabin@Sabins-MacBook-Air.local>
2024-02-29 13:04:30 +0545

    TAsk_1

---
+++ Diamond_Shape/src/Main.java
@@ -0,0 +1,28 @@
+public class Main{
+    public static void main(String[] args) {
+        int size = 4; // Adjust this to change the size of the diamond
+        int totalRows = size * 2 - 1;
+
+        // Print the top half of the diamond
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Print the bottom half of the diamond
+        for (int i = size - 1; i >= 1; i--) {
+            for (int j = 1; j <= size - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
